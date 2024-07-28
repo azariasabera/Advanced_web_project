26e9baba1722943bfe74741c9b0ddae172c481b9
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-28 16:12:50 +0300

    Done with server side's login and register post requests

--- root/server/app.js
+++ root/server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +18,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+if (process.env.NODE_ENV === 'development') {
+    var corsOptions = {
+      origin: 'http://localhost:3000',
+      optionsSuccessStatus: 200
+    };
+      app.use(cors(corsOptions));
+}
+
+// MongoDB setup -----------------------------------------------------------
+const mongoDB = 'mongodb://127.0.0.1:27017/testdb';
+mongoose.connect(mongoDB)
+        .then(() => console.log("MongoDB is connected!"))
+        .catch((error) => console.log(`Error has occured: ${error}`));
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error!!!"));
+// -------------------------------------------------------------------------¨
 
 module.exports = app;
--- root/server/routes/index.js
+++ root/server/routes/index.js
@@ -1,9 +1,112 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const {body, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+require('dotenv').config();
+require('../auth/validateToken');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const Users = require("../models/Users");
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET ROUTES
+
+
+// POST ROUTES
+
+router.post('/api/user/register', 
+  upload.none(),
+  body('email').isEmail(),
+  body('password')
+  .isLength({ min: 8 })
+  .matches(/[a-z]/).withMessage('at least one lowercase letter')
+  .matches(/[A-Z]/)
+  .matches(/[0-9]/)
+  .matches(/[~`!@#$%^&*()-_+={}[]|\;:"<>,.?|\/]/) // to include / used \ before /
+  
+  ,(req, res, next)=>{  
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send('Password is not strong enough');
+    }
+    Users.findOne({email: req.body.email})
+        .then(async (user) => {
+            if(!user){
+              const hashedPassword = await bcrypt.hash(req.body.password, 10)
+                let newUser = new Users({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashedPassword
+                });
+                newUser.save();
+                return res.send('User registered');
+                //res.redirect('/login.html');
+            } else {
+                return res.status(403).send('Email already in use');
+            }
+        }).catch((error) => {
+            res.status(500).send(`Error occured: ${error}`);
+        });
+});
+
+router.post('/api/user/login', 
+  upload.none(),
+  body('email').isEmail(),
+  body('password')
+  .isLength({ min: 8 })
+  .matches(/[a-z]/)
+  .matches(/[A-Z]/)
+  .matches(/[0-9]/)
+  .matches(/[~`!@#$%^&*()-_+={}[]|\;:"<>,.?|\/]/) // to include / used \ before /
+  
+  ,async (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send('Invalid credentials');
+    }
+    Users.findOne({email: req.body.email})
+        .then(async (user) => {
+            if(user){
+                if(await bcrypt.compare(req.body.password, user.password)){
+                    const tokenPayload = {
+                        email: user.email
+                    }
+                    jwt.sign(
+                      tokenPayload, 
+                      process.env.SECRET,
+                      {expiresIn: '1h'},
+                      (error, token) => {
+                          if(error){
+                              res.status(403).send(`Error in signing: ${error}`);
+                          } else {
+                              res.json({
+                                success: true,
+                                token: token});
+                          }
+                      })}
+                else{
+                    res.status(403).send('Invalid credentials');
+                }
+            }
+            else{
+                res.status(403).send('Invalid credentials');
+            }
+        }
+    ).catch((error) => {
+        res.status(500).send(`Error in .findOne: ${error}`);
+    });
 });
 
 module.exports = router;
